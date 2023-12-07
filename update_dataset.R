49b5ef51a6e0dcae5575fb5bf2efefeaeefddf7b
Paolo Dalena <paolodalena97@gmail.com>
2023-12-07 13:30:25 +0100

    manually fix data entry errors

--- update_dataset.R
+++ update_dataset.R
@@ -3,5 +3,10 @@
 update_dataset <- function(){
  df_calcetto <- googlesheets4::read_sheet("https://docs.google.com/spreadsheets/d/1rmI__ag7UU9BkPo05elDrvJvdOLT6noQGLOOPfuMKZo/edit?usp=sharing")
  message(paste0("Updated to ", Sys.Date()))
+ 
+ # correggo manualmente errori di compilazione
+ # NLP 06/12/2023 non ha fatto gol ma ne ha dichiarato 1
+ df_calcetto[72,32] <- 0
+ 
  return(df_calcetto)
 }
